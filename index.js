0bb8c78e957d0c44f87bff4686374d3c0750452e
kk0520 <karan.katyal07@gmail.com>
2020-07-01 13:37:17 -0400

    HTML Template Ready

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-let $title = $("#nameVal")
-let $searchBtn = $("#searchBtn")
+let $title = $("#nameVal");
+let $searchBtn = $("#searchBtn");
+
+
 
 $searchBtn.click (() => {
-    const Name = $title.val()
-    console.log(Name)
+    const name = $title.val();
+    console.log(name)
+    $.ajax({
+        url : 'http://www.omdbapi.com/',
+        type : 'GET',
+        dataType:'json',
+        data: $.param({ apikey: 'd8a727ad', s: name }),
+        success : handleSearchData,
+        error : handleApiError
+    });
 })
+
+function handleSearchData(data) {
+    //console.log("Data", data.Search);
+    let searchResults = data.Search;
+    // for(d in data.Search) {
+    //     console.log(data.Search[d].Title);
+    // }
+    searchResults.forEach(handleSingleSearchresult);
+}
+
+function handleApiError(request,error) {
+
+}
+
+function handleSingleSearchresult(result) {
+    console.log("Title : ", result.Title);
+}
