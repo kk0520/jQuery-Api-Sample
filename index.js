0a672ffaee900bc24743eaeab36a2899fea60c49
kk0520 <karan.katyal07@gmail.com>
2020-07-06 18:01:26 -0400

    Pagination Working

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 let $title = $("#nameVal");
 let $searchBtn = $("#searchBtn");
 
-
-
 $searchBtn.click (() => {
-    $("#target").empty();
+    $("#resultList").empty();
+    getMovieData(1)
+})
+
+function getMovieData(pageNumber){
     const name = $title.val();
     $.ajax({
         url : 'http://www.omdbapi.com/',
         type : 'GET',
         dataType:'json',
-        data: $.param({ apikey: 'd8a727ad', s: name }),
+        data: $.param({ apikey: 'd8a727ad', s: name, page: pageNumber}),
         success : handleSearchData,
         error : handleApiError
     });
-})
-
+}
 function handleSearchData(data) {
     let searchResults = data.Search;
     let totalRecords = data.totalResults;
     handlePagination(totalRecords);
     searchResults.forEach(handleSingleSearchresult);
-
-
 }
 
 function handleApiError(request,error) {
@@ -32,15 +31,22 @@ function handleApiError(request,error) {
 function handleSingleSearchresult(result) {
     let $iconString;
     if (result.Type=="movie"){
+        result.Type = "Movie"
         $iconString = "fa-film"
 
     }else if(result.Type=="series"){
+        result.Type = "Series"
         $iconString = "fa-tv"
     }
     else {
+        result.Type = "Game"
         $iconString = "fa-gamepad"
     }
     result.Icon = $iconString;
+
+    if(result.Poster == "N/A"){
+        result.Poster = "/Users/karankatyal/Documents/WebDev/jQuery-Api-Sample/Images/No-Image-Available.png"
+    }
     $.tmpl($("#displaytemplate"),result ).appendTo("#resultList");
 }
 
@@ -51,9 +57,14 @@ function handlePagination(totalRecords){
     else{
         $pageNumber = Math.ceil(totalRecords/10)
     }
-    console.log($pageNumber);
     for(let i=1; i<= $pageNumber; i++){
         var pageNumber = { "page"  : i};
         $.tmpl($("#paginationTemplate"),pageNumber ).appendTo("#paginationDiv");
     }
+}
+
+function pageClick(pageNumber){
+    $("#resultList").empty();
+    $("#paginationDiv").empty();
+    getMovieData(pageNumber);
 }
